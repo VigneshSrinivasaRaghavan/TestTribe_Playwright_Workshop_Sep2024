817483e72f8d2c345ac58cc33d3360b4319674b8
Vignesh Srinivasa Raghavan <40608099+VigneshSrinivasaRaghavan@users.noreply.github.com>
2024-09-21 13:04:43 +0200

    Added FirstTest and CodeGen Test

---
+++ tests/codeGenTest.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+  await page.getByPlaceholder('Username').click();
+  await page.getByPlaceholder('Username').fill('Admin');
+  await page.getByPlaceholder('Username').press('ArrowRight');
+  await page.getByPlaceholder('Username').press('ArrowRight');
+  await page.getByPlaceholder('Password').click();
+  await page.getByPlaceholder('Password').fill('admin123');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await page.waitForTimeout(5000);
+});
